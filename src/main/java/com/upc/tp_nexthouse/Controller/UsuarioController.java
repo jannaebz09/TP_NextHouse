8a66fc211955747629235390b9428b14e79e5797
jannaebz09 <pierinabazan123@gmail.com>
2024-11-22 21:50:09 -0500

    COMMIT ENTIDADES

--- src/main/java/com/upc/tp_nexthouse/Controller/UsuarioController.java
+++ src/main/java/com/upc/tp_nexthouse/Controller/UsuarioController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
+@CrossOrigin(origins = {"http://localhost:4200","http://http://3.144.130.45/","https://tp-nexthouse.onrender.com","https://tf-front-next-house.vercel.app/"})
 @RestController
 @RequestMapping("/NextHouse")
 public class UsuarioController {
